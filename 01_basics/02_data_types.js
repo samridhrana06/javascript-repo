c868547d107e90ef89558b4127c7d02528834589
samridhrana06 <106154154+samridhrana06@users.noreply.github.com>
2024-02-22 07:13:02 +0000

    data types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,23 @@
+"use strict"; // treat everything as new verison of JS
+
+// alert("Hello") we are using node js not browser
+
+console.log(3+3)
+
+console.log("Samridh") // code redability should be good
+
+let name = "Samridh"
+let age = 21
+let isLogged = false
+let state;
+// number => 2 to power 53
+// bigint 
+//string =>" "
+//boolean => true or false
+// null => it is a stand alone value
+// undefined => when there is nothing on it eg: let state;  it is not null but it dosent haven any value 
+// symbol 
+
+//boject
+
+console.log(typeof state)
