182aa8a39b398c228f9e2ffc11c99b616629e30c
samridhrana06 <106154154+samridhrana06@users.noreply.github.com>
2024-02-23 16:56:00 +0000

    conversion

--- 03_type_conversion.js
+++ 03_type_conversion.js
@@ -1,21 +1,57 @@
-let score = "samridh"
+// let score = "samridh"
 
-console.log(typeof score)
-// console.log(typeof (score))
+// console.log(typeof score)
+// // console.log(typeof (score))
 
-let valueInNumber = Number(score)
-console.log("After conversion")
-// console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log("After conversion")
+// // console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
-// "33" -> 33
-// "33abc" -> NaN
-// true => 1 false=>0
+// // "33" -> 33
+// // "33abc" -> NaN
+// // true => 1 false=>0
 
-let isLoggedin = 1;
+// let isLoggedin = 1;
 
-let isbooleanLoggedin = Boolean(isLoggedin)
-console.log(isbooleanLoggedin)
+// let isbooleanLoggedin = Boolean(isLoggedin)
+// console.log(isbooleanLoggedin)
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// operations
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = "Hello"
+// let str2 = " samridh"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+1+"2")
+
+// console.log(true) => true
+// console.log(+true) => 1
+
+// let num1, num2, num3
+// num1 = num2 = num3= 2+2
+
+let gameCounter = 100
+++gameCounter;
+
+console.log(gameCounter)
+
+
 
 
