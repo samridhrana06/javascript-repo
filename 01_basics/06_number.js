45a665eab76f37a8e7285c321ce16c5d41b04ea0
samridhrana06 <106154154+samridhrana06@users.noreply.github.com>
2024-02-24 12:09:45 +0000

    playing with numbers

---
+++ 01_basics/06_number.js
@@ -0,0 +1,36 @@
+const  score = 400
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(typeof(balance))
+
+// console.log(balance.toFixed(2))
+
+const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// +++++++++Maths+++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.5))
+// console.log(Math.min(3,5,1,7))
+// console.log(Math.max(4,5,7))
+
+// console.log(Math.random())
+
+// console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.random() * (max-min +1) + min)
+
+
