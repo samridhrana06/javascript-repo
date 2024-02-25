ff3ff5e53d9d70987ae9497f6b66da776d6a0606
samridhrana06 <106154154+samridhrana06@users.noreply.github.com>
2024-02-25 16:35:50 +0000

    functions eg shopping cart sending object and arrays in functions

---
+++ 02_basics/02_functions-part2.js
@@ -0,0 +1,32 @@
+// shopping cart 
+
+// function CalculateCartPrice(val1,val2,...num1){
+//     return num1
+// }
+
+// console.log(CalculateCartPrice(200,
+    // 400,500,700));
+
+// const user = {
+//     username: "sam",
+//     price: 123
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is${anyobject.price}`);
+// }
+
+// handleObject(user)
+
+// handleObject({
+//     username: "sam",
+//     price: 299
+
+// })
+
+const myNewArray = [200,400,100,600]
+function returnSecondValue(getArray){
+return getArray[1]
+}
+
+console.log(returnSecondValue([111,2222,3333,4,5,6]));
