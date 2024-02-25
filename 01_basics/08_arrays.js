81a2184a778632694ad9a8363a1496342d748a42
samridhrana06 <106154154+samridhrana06@users.noreply.github.com>
2024-02-25 10:29:28 +0000

    arrays

---
+++ 01_basics/08_arrays.js
@@ -0,0 +1,37 @@
+const myarr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","doga","nagraj"]
+
+
+const myarr2 = new Array(1,2,3,4)
+
+
+// console.log(myarr[4])
+// console.log(myHeros)
+
+//array methods
+
+// myarr.push(6)
+// myarr.push(7)
+// myarr.pop()
+
+// myarr.unshift(9)
+// myarr.shift()
+
+// console.log(myarr.includes(9))
+// console.log(myarr.indexOf(2))
+
+// const newarr = myarr.join()
+// console.log(myarr)
+// console.log(newarr)
+
+
+// slice and splice
+console.log("A",myarr);
+
+const myn1 = myarr.slice(1,3)
+
+console.log(myn1)
+
+console.log("B",myarr)
+
+console.log(myarr)
