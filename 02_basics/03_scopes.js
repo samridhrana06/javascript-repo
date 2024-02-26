d68802cd465fb1b11f0c30ae0b51147159421b1e
samridhrana06 <106154154+samridhrana06@users.noreply.github.com>
2024-02-26 07:05:41 +0000

    functions inside function scope

--- 02_basics/03_scopes.js
+++ 02_basics/03_scopes.js
@@ -1,14 +1,56 @@
-// var c = 300
+// // var c = 300
 
-let a = 300
-if(true)
-{
-    let a = 10
-    const b = 20 
-    // var c = 30 
-    console.log("Inner: ",a);
+// let a = 300
+// if(true)
+// {
+//     let a = 10
+//     const b = 20
+//     // var c = 30
+//     console.log("Inner: ",a);
+// }
+
+// console.log("outside: ",a);
+// // console.log(b);
+// // console.log(c);
+
+// nested scope
+
+function one() {
+  const userName = "Samridh";
+
+  function two() {
+    const website = "Youtube";
+    console.log(userName);
+  }
+//   console.log(website);
+
+  two();
+
 }
 
-console.log("outside: ",a);
-// console.log(b);
-// console.log(c);
+// one();
+
+
+if(true){
+    const username = "samridh"
+    if(username === "samridh"){
+        const website = " Youtube"
+        // console.log(username+website);
+    }
+    // console.log(website)
+}
+// console.log(username);
+
+
+///   -------------------------- Intresting ---------------------
+
+// function addone(num){
+// return num+1
+// }
+
+// addone(5)
+
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(5))
